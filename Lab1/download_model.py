f09615c2584b2b9e023afdde4a6a8fd9efdad04d
Patrick Shitou Zhang <stzhang@link.cuhk.edu.hk>
2026-02-28 15:46:25 +0800

    Update: download_model.py

--- Lab1/download_model.py
+++ Lab1/download_model.py
@@ -3,21 +3,21 @@
 
 from huggingface_hub import snapshot_download
 
-# print("Downloading Qwen3-ASR-0.6B model...")
-# snapshot_download(
-#     repo_id="Qwen/Qwen3-ASR-0.6B",
-#     local_dir="./Qwen3-ASR-0.6B",
-#     local_dir_use_symlinks=False
-# )
-# print("Qwen3-ASR-0.6B Download complete!")
+print("Downloading Qwen3-ASR-0.6B model...")
+snapshot_download(
+    repo_id="Qwen/Qwen3-ASR-0.6B",
+    local_dir="./Qwen3-ASR-0.6B",
+    local_dir_use_symlinks=False
+)
+print("Qwen3-ASR-0.6B Download complete!")
 
-# print("Downloading Qwen3-TTS-12Hz-0.6B-Base model...")
-# snapshot_download(
-#     repo_id="Qwen/Qwen3-TTS-12Hz-0.6B-Base",
-#     local_dir="./Qwen3-TTS-12Hz-0.6B-Base",
-#     local_dir_use_symlinks=False
-# )
-# print("Qwen3-TTS-12Hz-0.6B-Base Download complete!")
+print("Downloading Qwen3-TTS-12Hz-0.6B-Base model...")
+snapshot_download(
+    repo_id="Qwen/Qwen3-TTS-12Hz-0.6B-Base",
+    local_dir="./Qwen3-TTS-12Hz-0.6B-Base",
+    local_dir_use_symlinks=False
+)
+print("Qwen3-TTS-12Hz-0.6B-Base Download complete!")
 
 print("Downloading Qwen3-4B-quantized.w4a16 model...")
 snapshot_download(
